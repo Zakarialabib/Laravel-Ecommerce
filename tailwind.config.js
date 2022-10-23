44f582275b35ee32814ac1a677c2d425976d78d8
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2022-10-23 18:04:31 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.php",
+    "./resources/**/*.html",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
