8781f3ee4e263e0e00ba41c4cf5e24d43bb04061
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2022-12-27 16:07:01 +0100

    fix

--- resources/views/livewire/admin/section/create.blade.php
+++ resources/views/livewire/admin/section/create.blade.php
@@ -1,82 +1,88 @@
 <div>
     <x-modal wire:model="createSection">
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        <x-slot name="title">
+            {{ __('Create Section') }}
+        </x-slot>
 
-        <form enctype="multipart/form-data" wire:submit.prevent="save">
-            <div class="lg:w-1/2 sm:w-full px-2">
-                <x-label for="language_id" :value="__('Language')" />
-                <x-select-list
-                    class="block bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-gray-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500"
-                    id="language_id" name="language_id" wire:model="section.language_id" :options="$this->listsForFields['languages']" />
-                <x-input-error :messages="$errors->get('section.language_id')" for="section.language_id" class="mt-2" />
-            </div>
+        <x-slot name="content">
+            <!-- Validation Errors -->
+            <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-            <div class="lg:w-1/2 sm:w-full px-2">
-                <x-label for="page" :value="__('Page')" />
-                <select wire:model="section.page"
-                    class="p-3 leading-5 bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-zinc-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500  lang"
-                    name="page">
-                    <option value="" selected>{{ __('Select a Page') }}</option>
-                    <option value="1">{{ __('Home Page') }}</option>
-                    <option value="2">{{ __('About Page') }}</option>
-                    <option value="3">{{ __('Partner Page') }}</option>
-                    <option value="4">{{ __('Blog Page') }}</option>
-                    <option value="5">{{ __('Service Page') }}</option>
-                    <option value="7">{{ __('Contact Page') }}</option>
-                    <option value="8">{{ __('Products Page') }}</option>
-                    <option value="9">{{ __('Privacy Page') }}</option>
-                </select>
-                <x-input-error :messages="$errors->get('section.page')" for="section.page" class="mt-2" />
-            </div>
+            <form enctype="multipart/form-data" wire:submit.prevent="save">
+                <div class="lg:w-1/2 sm:w-full px-2">
+                    <x-label for="language_id" :value="__('Language')" />
+                    <x-select-list
+                        class="block bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-gray-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500"
+                        id="language_id" name="language_id" wire:model="section.language_id" :options="$this->listsForFields['languages']" />
+                    <x-input-error :messages="$errors->get('section.language_id')" for="section.language_id" class="mt-2" />
+                </div>
 
-            <div class="lg:w-1/2 sm:w-full px-2">
-                <x-label for="title" :value="__('Title')" />
-                <input type="text" name="title" wire:model.lazy="section.title"
-                    class="p-3 leading-5 bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-zinc-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500 "
-                    placeholder="{{ __('Title') }}" value="{{ old('title') }}">
-                <x-input-error :messages="$errors->get('section.title')" for="section.title" class="mt-2" />
-            </div>
-            <div class="lg:w-1/2 sm:w-full px-2">
-                <x-label for="subtitle" :value="__('Subtitle')" />
-                <input type="text" name="subtitle" wire:model.lazy="section.subtitle"
-                    class="p-3 leading-5 bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-zinc-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500 "
-                    placeholder="{{ __('Subtitle') }}" value="{{ old('subtitle') }}">
-                <x-input-error :messages="$errors->get('section.subtitle')" for="section.subtitle" class="mt-2" />
-            </div>
-            <div class="w-full px-2">
-                <x-label for="custom_html_1" :value="__('Custom html')" />
-                <textarea type="text" name="custom_html_1" wire:model.lazy="section.custom_html_1"
-                    class="p-3 leading-5 bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-zinc-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500 "
-                    placeholder="{{ __('Custom html') }}">
+                <div class="lg:w-1/2 sm:w-full px-2">
+                    <x-label for="page" :value="__('Page')" />
+                    <select wire:model="section.page"
+                        class="p-3 leading-5 bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-zinc-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500  lang"
+                        name="page">
+                        <option value="" selected>{{ __('Select a Page') }}</option>
+                        <option value="1">{{ __('Home Page') }}</option>
+                        <option value="2">{{ __('About Page') }}</option>
+                        <option value="3">{{ __('Partner Page') }}</option>
+                        <option value="4">{{ __('Blog Page') }}</option>
+                        <option value="5">{{ __('Service Page') }}</option>
+                        <option value="7">{{ __('Contact Page') }}</option>
+                        <option value="8">{{ __('Products Page') }}</option>
+                        <option value="9">{{ __('Privacy Page') }}</option>
+                    </select>
+                    <x-input-error :messages="$errors->get('section.page')" for="section.page" class="mt-2" />
+                </div>
+
+                <div class="lg:w-1/2 sm:w-full px-2">
+                    <x-label for="title" :value="__('Title')" />
+                    <input type="text" name="title" wire:model.lazy="section.title"
+                        class="p-3 leading-5 bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-zinc-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500 "
+                        placeholder="{{ __('Title') }}" value="{{ old('title') }}">
+                    <x-input-error :messages="$errors->get('section.title')" for="section.title" class="mt-2" />
+                </div>
+                <div class="lg:w-1/2 sm:w-full px-2">
+                    <x-label for="subtitle" :value="__('Subtitle')" />
+                    <input type="text" name="subtitle" wire:model.lazy="section.subtitle"
+                        class="p-3 leading-5 bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-zinc-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500 "
+                        placeholder="{{ __('Subtitle') }}" value="{{ old('subtitle') }}">
+                    <x-input-error :messages="$errors->get('section.subtitle')" for="section.subtitle" class="mt-2" />
+                </div>
+                <div class="w-full px-2">
+                    <x-label for="custom_html_1" :value="__('Custom html')" />
+                    <textarea type="text" name="custom_html_1" wire:model.lazy="section.custom_html_1"
+                        class="p-3 leading-5 bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-zinc-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500 "
+                        placeholder="{{ __('Custom html') }}">
                 </textarea>
-                <x-input-error :messages="$errors->get('section.custom_html_1')" for="section.custom_html_1" class="mt-2" />
-            </div>
-            <div class="w-full px-2">
-                <x-label for="content" :value="__('Description')" />
-                <x-input.rich-text wire:model.lazy="section.content" id="description" />
-                <x-input-error :messages="$errors->get('section.content')" for="section.content" class="mt-2" />
-            </div>
-            <div class="w-full px-2">
-                <x-label for="image" :value="__('Image')" />
-                <x-fileupload wire:model="image" :file="$image" accept="image/jpg,image/jpeg,image/png" />
-                <p class="help-block text-info">
-                    {{ __('Upload 670X418 (Pixel) Size image for best quality. Only jpg, jpeg, png image is allowed.') }}
-                </p>
-                <x-input-error :messages="$errors->get('section.image')" for="section.image" class="mt-2" />
-            </div>
-            <div class="lg:w-1/2 sm:w-full px-2">
-                <x-label for="video" :value="__('Video')" />
-                <input type="text" name="video" wire:model.lazy="section.video"
-                    class="p-3 leading-5 bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-zinc-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500 "
-                    placeholder="{{ __('Video') }}" value="{{ old('video') }}">
-                <x-input-error :messages="$errors->get('section.video')" for="section.video" class="mt-2" />
-            </div>
-            <div class="w-full px-2">
-                <x-button type="submit" primary>
-                    {{ __('Save') }}
-                </x-button>
-            </div>
-        </form>
+                    <x-input-error :messages="$errors->get('section.custom_html_1')" for="section.custom_html_1" class="mt-2" />
+                </div>
+                <div class="w-full px-2">
+                    <x-label for="content" :value="__('Description')" />
+                    <x-input.rich-text wire:model.lazy="section.content" id="description" />
+                    <x-input-error :messages="$errors->get('section.content')" for="section.content" class="mt-2" />
+                </div>
+                <div class="w-full px-2">
+                    <x-label for="image" :value="__('Image')" />
+                    <x-fileupload wire:model="image" :file="$image" accept="image/jpg,image/jpeg,image/png" />
+                    <p class="help-block text-info">
+                        {{ __('Upload 670X418 (Pixel) Size image for best quality. Only jpg, jpeg, png image is allowed.') }}
+                    </p>
+                    <x-input-error :messages="$errors->get('section.image')" for="section.image" class="mt-2" />
+                </div>
+                <div class="lg:w-1/2 sm:w-full px-2">
+                    <x-label for="video" :value="__('Video')" />
+                    <input type="text" name="video" wire:model.lazy="section.video"
+                        class="p-3 leading-5 bg-white dark:bg-dark-eval-2 text-gray-700 dark:text-gray-300 rounded border border-zinc-300 mb-1 text-sm w-full focus:shadow-outline-blue focus:border-blue-500 "
+                        placeholder="{{ __('Video') }}" value="{{ old('video') }}">
+                    <x-input-error :messages="$errors->get('section.video')" for="section.video" class="mt-2" />
+                </div>
+                <div class="w-full px-2">
+                    <x-button type="submit" primary>
+                        {{ __('Save') }}
+                    </x-button>
+                </div>
+            </form>
+        </x-slot>
     </x-modal>
 </div>
