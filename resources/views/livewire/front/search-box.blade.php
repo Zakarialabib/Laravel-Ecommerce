ced4bae5ba11f55eec18b22f71fb670dab0e0372
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-01-19 18:10:28 +0100

    Update search-box.blade.php

--- resources/views/livewire/front/search-box.blade.php
+++ resources/views/livewire/front/search-box.blade.php
@@ -1,26 +1,21 @@
 <div>
-    <div x-data="{ searchBox: {{ $searchBox ? 'true' : 'false' }} }"
-    class="relative w-full mx-4"  @click.away="searchBox = false">
-        <div class="flex items-center max-w-md rounded-lg mr-5">
+    <div x-data="{ searchBox: {{ $searchBox ? 'true' : 'false' }} }" class="relative w-full mr-6" @click.away="searchBox = false">
+        <div class="flex items-center max-w-md rounded-lg">
             <button type="button" @click="searchBox = !searchBox"
                 class="flex items-center justify-center w-10 h-10 text-gray-100 rounded-l-lg focus:outline-none">
                 <i class="fa fa-search mr-5 text-gray-100"></i>
             </button>
             <input type="text" wire:model="search" placeholder="{{ __('Search for products') }}" autocomplete=""
-                class="w-full border-0 focus:ring-transparent bg-gray-100 text-gray-900 text-xs focus:outline-none py-2 mr-4 rounded-md">
+                class="w-full border-0 focus:ring-transparent bg-gray-100 text-gray-900 text-xs focus:outline-none py-2 px-4 rounded-md">
             <button type="button" wire:click="clearSearch"
-                    class="h-full absolute z-20 top-0 right-0 flex items-center text-gray-800 focus:outline-none">
+                class="h-full absolute z-20 top-0 right-0 flex items-center text-gray-800 focus:outline-none">
                 <i class="fa fa-times mr-5 text-gray-900"></i>
             </button>
         </div>
         @if (!empty($search))
-            <div 
-                x-transition:enter="transition-all duration-300"
-                x-transition:enter-start="opacity-0"
-                x-transition:enter-end="opacity-100"
-                x-transition:leave="transition-all" 
-                x-transition:leave-start="opacity-25"
-                x-transition:leave-end="opacity-0 hidden"
+            <div x-transition:enter="transition-all duration-300" x-transition:enter-start="opacity-0"
+                x-transition:enter-end="opacity-100" x-transition:leave="transition-all"
+                x-transition:leave-start="opacity-25" x-transition:leave-end="opacity-0 hidden"
                 class="absolute top-0 left-0 w-full mt-12 bg-white rounded-md shadow-xl overflow-y-auto max-h-52 z-50"
                 wire:click="hideSearchResults">
                 <ul>
