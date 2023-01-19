e09a2700e9763ed6cb4ad687f7b359c67aa78b73
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-01-19 18:47:48 +0100

    update

--- resources/views/livewire/front/search-box.blade.php
+++ resources/views/livewire/front/search-box.blade.php
@@ -1,8 +1,8 @@
 <div>
     <div x-data="{ searchBox: {{ $searchBox ? 'true' : 'false' }} }" class="relative w-full" @click.away="searchBox = false">
-        <div class="flex items-center max-w-md mx-4">
+        <div class="flex items-center max-w-md mx-4 border-gray-200 hover:border-red-400 focus:border-red-400">
             <button type="button" @click="searchBox = !searchBox"
-                class="h-full absolute z-20 top-0 px-2 flex items-center bg-gray-800 border-gray-200 hover:border-red-400 focus:border-red-400 focus:outline-none">
+                class="h-full absolute z-20 top-0 px-2 flex items-center bg-gray-800 focus:outline-none">
                 <i class="fa fa-search text-gray-100"></i>
             </button>
             <input type="text" wire:model="search" placeholder="{{ __('Search for products') }}" autocomplete=""
