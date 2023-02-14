43ef7834cd6dcc82a76c73838310e17b4f0eb291
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-02-14 22:17:31 +0100

    Making connection with myStockMaster

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    public function authenticate(Request $request)
+{
+    $url = $request->input('url');
+    $token = $request->input('token');
+
+    $client = new Client();
+    $response = $client->request('GET', $url.'/sanctum/csrf-cookie');
+    $response = $client->request('POST', $url.'/login', [
+        'headers' => [
+            'Accept' => 'application/json',
+            'X-Requested-With' => 'XMLHttpRequest',
+        ],
+        'json' => [
+            'email' => 'your-email@example.com',
+            'password' => 'your-password',
+        ],
+    ]);
+    $response = $client->request('GET', $url.'/api/user', [
+        'headers' => [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$token,
+        ],
+    ]);
+
+    $user = json_decode($response->getBody(), true);
+    
+    // Store the API token and authenticated user in the session
+    session(['api_token' => $token, 'api_user' => $user]);
+    
+    return response()->json(['data' => 'Authenticated successfully']);
+}
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('auth', [\App\Http\Controllers\Api\AuthController::class, 'authenticate']);
