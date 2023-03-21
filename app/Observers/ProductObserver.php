572ab4410ab98502d039bcd3137e623a33dce7e5
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-03-21 18:20:51 +0000

    Create ProductObserver.php

---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Observers;
+
+use App\Models\Product;
+use App\Models\Redirect;
+use App\Enums\RedirectionStatus;
+
+class ProductObserver
+{
+    /**
+     * Handle the Product "updating" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function updating(Product $product)
+    {
+        // Check if the product's slug has changed
+        if ($product->isDirty('slug')) {
+            $oldSlug = $product->getOriginal('slug');
+            $newSlug = $product->getAttribute('slug');
+
+            // Create a redirection entry for the old slug
+            Redirect::create([
+                'old_url' => $oldSlug,
+                'new_url' => $newSlug,
+                'http_status_code' => RedirectionStatus::MOVED_PERMANENTLY,
+            ]);
+
+            // Update the product's URL to use the new slug
+            $product->setAttribute('slug', $newSlug);
+        }
+    }
+}
