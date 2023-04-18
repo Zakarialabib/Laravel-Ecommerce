f43783067fdfbc0841051876b9d8400deee7ce1d
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2023-04-18 14:00:53 +0000

    changing the loading svg

--- resources/views/components/loading-mask.blade.php
+++ resources/views/components/loading-mask.blade.php
@@ -1,58 +1,18 @@
-<div x-data="loadingMask" x-show="!pageLoaded"  class="fixed inset-0 z-[100] flex items-center justify-center bg-gray-900 opacity-75">
-    <svg width="105" height="105" viewBox="0 0 105 105" xmlns="http://www.w3.org/2000/svg" fill="#fff">
-        <circle cx="12.5" cy="12.5" r="12.5">
-            <animate attributeName="fill-opacity"
-             begin="0s" dur="1s"
-             values="1;.2;1" calcMode="linear"
-             repeatCount="indefinite" />
-        </circle>
-        <circle cx="12.5" cy="52.5" r="12.5" fill-opacity=".5">
-            <animate attributeName="fill-opacity"
-             begin="100ms" dur="1s"
-             values="1;.2;1" calcMode="linear"
-             repeatCount="indefinite" />
-        </circle>
-        <circle cx="52.5" cy="12.5" r="12.5">
-            <animate attributeName="fill-opacity"
-             begin="300ms" dur="1s"
-             values="1;.2;1" calcMode="linear"
-             repeatCount="indefinite" />
-        </circle>
-        <circle cx="52.5" cy="52.5" r="12.5">
-            <animate attributeName="fill-opacity"
-             begin="600ms" dur="1s"
-             values="1;.2;1" calcMode="linear"
-             repeatCount="indefinite" />
-        </circle>
-        <circle cx="92.5" cy="12.5" r="12.5">
-            <animate attributeName="fill-opacity"
-             begin="800ms" dur="1s"
-             values="1;.2;1" calcMode="linear"
-             repeatCount="indefinite" />
-        </circle>
-        <circle cx="92.5" cy="52.5" r="12.5">
-            <animate attributeName="fill-opacity"
-             begin="400ms" dur="1s"
-             values="1;.2;1" calcMode="linear"
-             repeatCount="indefinite" />
-        </circle>
-        <circle cx="12.5" cy="92.5" r="12.5">
-            <animate attributeName="fill-opacity"
-             begin="700ms" dur="1s"
-             values="1;.2;1" calcMode="linear"
-             repeatCount="indefinite" />
-        </circle>
-        <circle cx="52.5" cy="92.5" r="12.5">
-            <animate attributeName="fill-opacity"
-             begin="500ms" dur="1s"
-             values="1;.2;1" calcMode="linear"
-             repeatCount="indefinite" />
-        </circle>
-        <circle cx="92.5" cy="92.5" r="12.5">
-            <animate attributeName="fill-opacity"
-             begin="200ms" dur="1s"
-             values="1;.2;1" calcMode="linear"
-             repeatCount="indefinite" />
-        </circle>
+@props([
+    'size' => 'lg',
+    'class' => '',
+    'sizing' => [
+        'sm' => 6,
+        'md' => 10,
+        'lg' => 14,
+        'xl' => 24,
+        'xxl' => 36
+    ],
+])
+<div x-data="loadingMask" x-show="!pageLoaded"  class="fixed inset-0 z-[100] flex items-center justify-center bg-gray-600 opacity-75">
+    <svg class="animate-spin inline-block bw-spinner h-{{ $sizing[$size] }} w-{{ $sizing[$size] }} {{$class}}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
     </svg>
+    <span class="hidden w-6 w-10 w-14 w-24 w-36 h-6 h-10 h-14 h-24 h-36"></span>
 </div>
