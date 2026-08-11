b7dc397bee956f73e4dd4dae730c45cbcf60039c
webbrain-one <295484252+webbrain-one@users.noreply.github.com>
2026-08-11 18:34:08 +0300

    Add missing Admin controllers to fix route list error
    Resolves #15 by creating the missing ReportController and SmptController
    in app/Http/Controllers/Admin/. This fixes the ReflectionException thrown
    when running `php artisan route:list` due to undefined controller classes
    referenced in routes/admin.php.
    
    Patch generated by qwen3.6-35b-a3b via local API.

---
+++ app/Http/Controllers/Admin/SmptController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+class SmptController extends Controller
+{
+    public function index(): View|Factory
+    {
+        return view('admin.smtp.index');
+    }
+}
